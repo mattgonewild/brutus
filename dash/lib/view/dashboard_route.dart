3266ec3c096d3646405ae85f2d24bd0247fe6820
Matt Walker <human@mattgonewild.com>
2024-01-16 00:22:38 -0500

    feat(dash): :iphone: add dashboard layout constraints

--- dash/lib/view/dashboard_route.dart
+++ dash/lib/view/dashboard_route.dart
@@ -1,4 +1,3 @@
-import 'package:dash/view/widget/hello_world.dart';
 import 'package:flutter/material.dart';
 
 class DashboardRoute extends StatelessWidget {
@@ -6,6 +5,61 @@ class DashboardRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CenteredHelloWorld();
+    return LayoutBuilder(
+        builder: (context, constraints) => pickLayout(context, constraints));
+  }
+}
+
+Widget pickLayout(BuildContext context, BoxConstraints constraints) {
+  if (constraints.maxWidth < 1080) {
+    // mobile
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      return const DashboardMobilePortrait();
+    } else {
+      return const DashboardMobileLandscape();
+    }
+  } else {
+    // desktop
+    if (MediaQuery.of(context).orientation == Orientation.portrait) {
+      return const DashboardDesktopPortrait();
+    } else {
+      return const DashboardDesktopLandscape();
+    }
+  }
+}
+
+class DashboardMobilePortrait extends StatelessWidget {
+  const DashboardMobilePortrait({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.purple);
+  }
+}
+
+class DashboardMobileLandscape extends StatelessWidget {
+  const DashboardMobileLandscape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue);
+  }
+}
+
+class DashboardDesktopPortrait extends StatelessWidget {
+  const DashboardDesktopPortrait({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.orange);
+  }
+}
+
+class DashboardDesktopLandscape extends StatelessWidget {
+  const DashboardDesktopLandscape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red);
   }
 }
