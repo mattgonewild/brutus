56c624f95c7fed99bc5b5ff547716fff4e1e9a19
Matt Walker <human@mattgonewild.com>
2024-01-18 04:05:49 -0500

    feat(dash): :construction: start NodeRail

--- dash/lib/bloc/node/node_state.dart
+++ dash/lib/bloc/node/node_state.dart
@@ -1,8 +1,10 @@
 part of 'node_bloc.dart';
 
 final class NodeState extends Equatable {
-  const NodeState();
+  const NodeState({this.nodeCount = 0});
+
+  final int nodeCount;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [nodeCount];
 }
--- dash/lib/view/widget/rail_node.dart
+++ dash/lib/view/widget/rail_node.dart
@@ -1,7 +1,23 @@
+import 'package:dash/bloc/node/node_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+// TODO: add sortable header
 class NodeRail extends StatelessWidget {
   const NodeRail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NodeBloc, NodeState>(buildWhen: (previous, current) => previous != current,
+      builder: (context, state) => ListView.builder(itemCount: state.nodeCount,
+        itemBuilder: (context, index) => const AspectRatio(aspectRatio: 1.2, child: AnimatedSwitcher(duration: Duration(milliseconds: 200), child: NodeRailCard()))
+      )
+    );
+  }
+}
+
+class NodeRailCard extends StatelessWidget {
+  const NodeRailCard({super.key});
 
   @override
   Widget build(BuildContext context) {
